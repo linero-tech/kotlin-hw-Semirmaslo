9e26704dd53bf49e5e4bc203f465536e432fbeb6
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-10 14:12:34 +0100

    Solution Task 8 101

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -2,5 +2,15 @@ package lms_126
 
 fun task8(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT X
+        IF X % 3 == 0
+        OUTPUT Foo
+        IF X % 5 == 0
+        OUTPUT Bar
+        IF X % 3 == 0 and X % 5 == 0
+        OUTPUT FooBar
+        ELSE OUTPUT Qix
+        
+    """
 }
