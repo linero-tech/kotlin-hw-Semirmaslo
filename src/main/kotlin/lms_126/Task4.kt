cb8841a08ffe7ead73c4817514e780010884cc67
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-10 11:39:56 +0100

    update solution 4 101

--- src/main/kotlin/lms_126/Task4.kt
+++ src/main/kotlin/lms_126/Task4.kt
@@ -3,6 +3,7 @@ package lms_126
 fun task4(): String {
 
     return """
+        INPUT highest value of X and Y
         if X has a higher value then Y = print X
         else if Y has a higher value then X = print Y
         
--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -2,5 +2,8 @@ package lms_126
 
 fun task5(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        
+        
+    """
 }
