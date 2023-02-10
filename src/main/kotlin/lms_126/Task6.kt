e17c5767ad1453d7f89fc8c9a49a99389030cb82
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-10 13:11:07 +0100

    solution task 6 101

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -2,5 +2,10 @@ package lms_126
 
 fun task6(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT number in range 100
+        IF number multiples with 5 = print number
+        ELSE print error
+        
+    """
 }
