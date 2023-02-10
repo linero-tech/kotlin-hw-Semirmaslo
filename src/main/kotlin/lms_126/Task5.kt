8b7f3a0377fd1fbccca9923c61c0e362cb69fa10
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-10 11:59:38 +0100

    Solution Task 5 101

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -3,7 +3,15 @@ package lms_126
 fun task5(): String {
 
     return """
-        
+        INPUT value of X
+        IF value of X is 1 OUTPUT Monday
+        IF value of X is 2 OUTPUT Tuesday
+        IF value of X is 3 OUTPUT Wednesday
+        IF value of X is 4 OUTPUT Thursday
+        IF value of X is 5 OUTPUT Friday
+        IF value of X is 6 OUTPUT Saturday
+        IF value of X is 7 OUTPUT Sunday
+        IF value of X is 8 OUTPUT error message
         
     """
 }
