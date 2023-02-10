60e8c95a52a2ca19374374a2a0cfbe64dedd3604
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-10 11:26:53 +0100

    Update Task4.kt

--- src/main/kotlin/lms_126/Task4.kt
+++ src/main/kotlin/lms_126/Task4.kt
@@ -3,9 +3,8 @@ package lms_126
 fun task4(): String {
 
     return """
-        IF X have higher value then Y print X
-        IF Y have higher value then X print Y
-        print highest value
+        if X has a higher value then Y = print X
+        else if Y has a higher value then X = print Y
         
     """
 }
