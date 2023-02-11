93d55dade2a35001019af49fb10fe927c519a0bf
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-11 09:53:51 +0100

    Solution to task 9 101

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -2,5 +2,15 @@ package lms_126
 
 fun task9(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """  
+        INPUT CC to machine
+        INPUT CC code
+        INPUT amount to withdraw
+        IF amount to withdraw > users balance
+        OUTPUT request new amount
+        ELSE output money to user
+        Update the users account
+        Ask if the CC user wants payslip if (YES) = print payslip
+        OUTPUT card
+    """
 }
