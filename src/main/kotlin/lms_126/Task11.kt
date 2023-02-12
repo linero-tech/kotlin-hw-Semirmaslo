f2a760536906d4fdf91eb1c69e61634b07e37a82
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-12 15:06:40 +0100

    Solution task 11 101

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,10 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        Separate the unit from Magnitude
+        Transfer magnitude to F
+        ELSE the unit is F
+        Transfer magnitude
+    """
 }
