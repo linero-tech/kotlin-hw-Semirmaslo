e04f7bcf8c291a9893d9f58c09d2021d2e832901
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-20 17:49:45 +0100

    Solution task 3 modul 102

--- src/main/kotlin/lms_127/Task3.kt
+++ src/main/kotlin/lms_127/Task3.kt
@@ -2,5 +2,12 @@ package lms_127
 
 fun task3(radius: Double): Double {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = 2.0 * radius * 3.1416
+    return  result
+
+}
+
+fun main() {
+    val result = task3(radius = 2.0)
+    println(result)
 }
