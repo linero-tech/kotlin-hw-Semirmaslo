df5c0d2f645eae5291ae8230cc6e82853f8a74e9
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-26 12:26:17 +0100

    update task 8

--- src/main/kotlin/lms_127/Task8.kt
+++ src/main/kotlin/lms_127/Task8.kt
@@ -2,10 +2,10 @@ package lms_127
 
 fun task8(sentence: String, character: Char): Int {
 
-    val result = return sentence.count, { it == character }
+    val result = return sentence.count { it == character }
     return result
 }
 fun main(){
-    println(task8(sentence = "I code in KOTLIN", character = 'i'))
+    println(task8(sentence = "I code in KOTLIN", character = 'I'))
 }
 
