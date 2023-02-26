244a5d6133ce1efccd43e78589a99c78e86a900a
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-26 12:34:44 +0100

    update task 9 102

--- src/main/kotlin/lms_127/Task9.kt
+++ src/main/kotlin/lms_127/Task9.kt
@@ -2,7 +2,7 @@ package lms_127
 
 fun task9(sentence: String, character: Char): Boolean {
 
-    val result = sentence.contains(character, ignorCase = true)
+    val result = sentence.contains(character,ignoreCase = true)
     return result
 
 }
