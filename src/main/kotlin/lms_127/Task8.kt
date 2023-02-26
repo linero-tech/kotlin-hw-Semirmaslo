ba8c8625c328fe5a181704adb00954763e3078c4
Semirmaslo <124594207+Semirmaslo@users.noreply.github.com>
2023-02-26 12:07:52 +0100

    solution task 8 module 102

--- src/main/kotlin/lms_127/Task8.kt
+++ src/main/kotlin/lms_127/Task8.kt
@@ -2,5 +2,10 @@ package lms_127
 
 fun task8(sentence: String, character: Char): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = return sentence.count, { it == character }
+    return result
 }
+fun main(){
+    println(task8(sentence = "I code in KOTLIN", character = 'i'))
+}
+
